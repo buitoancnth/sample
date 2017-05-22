ffa2ac1af49f3f45385d05f2f19b89217ef5f3cb
Bui Toan <buitoanncth@gmail.com>
2017-05-22 14:25:27 +0700

    tiep

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,24 +56,17 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "sample_app_#{Rails.env}"
-  config.action_mailer.perform_caching = false
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+   address:              'smtp.gmail.com',
+   port:                 587,
+   domain:               'enigmatic-hollows-97688.herokuapp.com',
+   user_name:            'buitoanncth@gmail.com',
+   password:             'buitoan1',
+   authentication:       'plain',
+   enable_starttls_auto: true  
+}
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  host = 'enigmatic-hollows-97688.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.gmail.com',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => 'buitoanncth@gmail.com',
-    :password       => 'buitoan1',
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
   
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
