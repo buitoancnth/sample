37f10f78dc05c86252a279b4d649bbbc1b8535ca
Bui Toan <buitoanncth@gmail.com>
2017-05-22 14:48:16 +0700

    aaa

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,14 +57,14 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
 config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-   address:              'smtp.gmail.com',
-   port:                 587,
-   domain:               'enigmatic-hollows-97688.herokuapp.com',
-   user_name:            'buitoanncth@gmail.com',
-   password:             'buitoan1',
-   authentication:       'plain',
-   enable_starttls_auto: true  
+# SMTP settings for mailgun
+ActionMailer::Base.smtp_settings = {
+  :port           => 587,
+  :address        => "smtp.mailgun.org",
+  :domain         => ENV['domain'],
+  :user_name      => ENV['username'],
+  :password       => ENV['password'],
+  :authentication => :plain,
 }
 
   
